6143021b42cda21ab349edf60671cace0f064a5d
RudychikFedorV <rudychik.fedor@gmail.com>
2024-06-21 09:09:37 +0300

    design section

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ function navToggle() {
     document.getElementById("headerUl").classList.toggle("_active");
     document.getElementById("headerBackdrop").classList.toggle("_active");
 }
+
+AOS.init();
